01481423c312c6a3ce993f6d121dbfa6df562299
Yuzhe <yuzhe.c85@gmail.com>
2014-01-09 16:54:12 +0800

    adjust honorees in index

--- public/js/wheel_and_parallax.js
+++ public/js/wheel_and_parallax.js
@@ -86,41 +86,5 @@ $(function() {
         }; 
     }
 
-    //視差
-    $('div#stage-1 .level-1').parallax({ "start": 0, "stop":430, "stop_point": 230 });
-    $('div#stage-1 .level-3').parallax({ "start": 0, "stop":350, "stop_point": -200 });
-
-    $('div#stage-2 .level-3').parallax({ "start": 300, "stop": 450 });
-
-    $('div#stage-3 #item-5').parallax({ "start": $("div#news").offset().top - 350, "stop": $("div#stage-3").offset().top - 480, "stop_point": 250});
-    $('div#stage-3 #item-4').parallax({ "start": $("div#news").offset().top - 300, "stop": $("div#stage-3").offset().top - 430, "stop_point": 250});
-    $('div#stage-3 #item-3').parallax({ "start": $("div#news").offset().top - 250, "stop": $("div#stage-3").offset().top - 380, "stop_point": 250});
-    $('div#stage-3 #item-2').parallax({ "start": $("div#news").offset().top - 200, "stop": $("div#stage-3").offset().top - 330, "stop_point": 250});
-    $('div#stage-3 #item-1').parallax({ "start": $("div#news").offset().top - 150, "stop": $("div#stage-3").offset().top - 280, "stop_point": 250});
-
-    $('div#stage-4 .level-1').parallax({ "start": 1800, "stop":2000, "stop_point": 190 });
-    $('div#stage-4 .level-3').parallax({ "start": 1950, "stop":2290, "stop_point": 405 });
-    $('div#stage-4 .level-2').parallax({ "start": 1950, "stop":2290, "stop_point": 528 });
-
-    //$('div#service').parallax({ "start": 2350, "stop":2850, "stop_point": 0 });
-    //$('div#stage-5').parallax({ "start": 2500, "stop":2945, "stop_point": 0 });
-    $('div#stage-5 .level-2').parallax({ "start": 2500, "stop":3400, "stop_point": 120 });
-    $('div#stage-5 .level-3').parallax({ "start": 2700, "stop":3400, "stop_point": 240 });
-
-    $('div#stage-6 .level-2').parallax({ "start": 3900, "stop":4000, "stop_point": 60 });
-    $('div#stage-6 .level-3').parallax({ "start": 3900, "stop":4000, "stop_point": 135 });
-
-    $('div#stage-7 .level-1').parallax({ "start": 4500, "stop":5200, "stop_point": 105 });
-    $('div#stage-7 .level-2').parallax({ "start": 4500, "stop":5200, "stop_point": 195 });
-
-    $('div#stage-8 .level-2').parallax({ "start": 4600, "stop":5500, "stop_point": 10 });
-    $('div#stage-8 .level-3').parallax({ "start": 5400, "stop":5800, "stop_point": 30 });
-
-    $('div#stage-9 #item-3').parallax({ "start": 6000, "stop":6200, "stop_point": 195 });
-    $('div#stage-9 #item-2').parallax({ "start": 6100, "stop":6300, "stop_point": 195 });
-    $('div#stage-9 #item-1').parallax({ "start": 6200, "stop":6400, "stop_point": 195 });
-
-    //$('div#stage-10 .level-2').parallax({ "start": 6700, "stop":7080, "stop_point": 70 });
-    $('div#stage-10 .level-3').parallax({ "start": 6700, "stop":7080, "stop_point": 24 });
 });
 
